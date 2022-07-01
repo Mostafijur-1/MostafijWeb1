c618c51cef10b597d67e0f86ca4afd47d0a6523d
Mostafijur Rahman <72193044+Mostafijur-1@users.noreply.github.com>
2022-07-01 18:36:59 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var x = document.querySelector(".navbar-menu");
+
+function navIcon() {
+   if (x.style.display==="block"){
+    x.style.display = "none";
+   }
+
+   else{
+    x.style.display = "block";
+   }
+}
